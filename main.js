d81140fee367acc22ce6693464cf2aa72e6e4773
laurinha <114171279+cyb3rlaura@users.noreply.github.com>
2023-10-10 09:31:49 -0300

    main.js

--- main.js
+++ main.js
@@ -2,4 +2,4 @@ function tocaSomPom() {
   document.querySelector('#som_tecla_pom').play();
 }
 
-cont listaDeTeclas = document.querySelectorAll('.tecla')
+cont listaDeTeclas =document.querySelectorAll('.tecla')
