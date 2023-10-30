89d5df32da75d2c958bdc81145a30b6cc3e07e19
laurinha <114171279+cyb3rlaura@users.noreply.github.com>
2023-10-30 07:49:06 -0300

    main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 function tocasSomPom () {
 document.querySelector('#som_tecla_pom').play();
 }
@@ -17,20 +18,3 @@ console.log(contador);
 
 }
 
-
-function tocaSom(idElementoAudio){
-    document.querySelector(idElementoAudio).play();
-}
-
-const listaDeTeclas = document.querySelectorAll(".tecla");
-
-let contador = 0;
-
-while(contador < listaDeTeclas.length){
-    const efeito = listaDeTeclas[contador].classList[1];
-    const idAudio = "#som_"+efeito;
-    listaDeTeclas[contador].onclick = function (){
-        tocaSom(idAudio);
-    }
-    contador = contador + 1;  
-}
